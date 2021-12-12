cb876e4bd0a0a110827ede1f868ac6c2fc048411
andreaspencer <andreaaddawn@gmail.com>
2021-12-11 17:20:28 -0800

    models set up

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,16 @@
+const router = required('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getAllThoughts).post(createThought);
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions/').post(addReaction).delete(deleteReaction);
+
+module.exports = router;
